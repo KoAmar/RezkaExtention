3c354c49eb9c74d0915b02474d83a2090c3f41ee
KoAmar <43239620+KoAmar@users.noreply.github.com>
2021-08-15 18:15:07 +0300

    dry principle

--- scripts.js
+++ scripts.js
@@ -1,36 +1,26 @@
-// document.body.style.border = "5px solid red";
-
 let queryString = window.location.href;
 
 
 if (queryString.toLowerCase().includes("rezka")) {
     let player = document.querySelector('#oframecdnplayer');
 
-    document.onkeydown = function KeyPress(e) {
-        if (e.key === 'R' && e.shiftKey) {
-            for (let a of player.querySelectorAll('pjsdiv')) {
-                if (a.hasAttribute('f2id')) {
-                    for (let b of a.querySelectorAll('pjsdiv')) {
-                        if (b.innerHTML === 'Русский') {
-                            console.log(a)
-                            a.click()
-                        }
-                    }
+    function clickLang(lang) {
+        for (let a of player.querySelectorAll('pjsdiv')) {
+            if (a.hasAttribute('f2id')) {
+                if (a.textContent === lang) {
+                    a.click()
                 }
             }
         }
+    }
+
+    document.onkeydown = function KeyPress(e) {
+        if (e.key === 'R' && e.shiftKey) {
+            clickLang('Русский')
+        }
 
         if (e.key === 'E' && e.shiftKey) {
-            for (let a of player.querySelectorAll('pjsdiv')) {
-                if (a.hasAttribute('f2id')) {
-                    for (let b of a.querySelectorAll('pjsdiv')) {
-                        if (b.innerHTML === 'English') {
-                            console.log(a)
-                            a.click()
-                        }
-                    }
-                }
-            }
+            clickLang('English')
         }
     };
 }
