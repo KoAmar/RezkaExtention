06bf6c219d05911ea601ca0a12de34e7c3eb5caf
KoAmar <43239620+KoAmar@users.noreply.github.com>
2021-08-15 16:08:35 +0300

    language selector

--- borderify.js
+++ borderify.js
@@ -1,1 +1,24 @@
-document.body.style.border = "5px solid red";
+// document.body.style.border = "5px solid red";
+
+let queryString = window.location.href;
+
+
+if (queryString.toLowerCase().includes("hdrezka")) {
+    element;
+
+    player = document.querySelector('#oframecdnplayer');
+
+    for (let a of player.querySelectorAll('pjsdiv')) {
+        if (a.hasAttribute('f2id')) {
+            for (let b of a.querySelectorAll('pjsdiv')) {
+                if (b.innerHTML === 'Русский' || b.innerHTML === 'English') {
+                    console.log(b)
+
+                }
+            }
+        }
+    }
+
+    element.click();
+}
+
