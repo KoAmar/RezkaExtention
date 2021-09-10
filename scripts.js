da41e79a524ff8cd29d589ed27b09a57d9ee7e99
KoAmar <43239620+KoAmar@users.noreply.github.com>
2021-09-10 12:29:00 +0300

    new shortcuts and new user comfort features

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 let queryString = window.location.href;
 
-
 if (queryString.toLowerCase().includes("rezka")) {
     let player = document.querySelector('#oframecdnplayer');
+
+    const ccCheckInterval = setInterval(function () {
+        let cc = document.querySelector('#cdnplayer_control_cc > pjsdiv:nth-child(3)')
+        if (cc) {
+            cc.click();
+            setTimeout(cc.click(), 10)
+            clearInterval(ccCheckInterval);
+        }
+
+    }, 1000);
 
     function clickLang(lang) {
         for (let a of player.querySelectorAll('pjsdiv')) {
@@ -14,13 +23,41 @@ if (queryString.toLowerCase().includes("rezka")) {
         }
     }
 
-    document.onkeydown = function KeyPress(e) {
-        if (e.key === 'R' && e.shiftKey) {
-            clickLang('Русский')
-        }
 
-        if (e.key === 'E' && e.shiftKey) {
-            clickLang('English')
+    document.onkeydown = function KeyPress(e) {
+        if (e.shiftKey) {
+            switch (e.code) {
+                case 'KeyR': {
+                    clickLang('Русский');
+                    break;
+                }
+                case 'KeyE': {
+                    clickLang('English');
+                    break;
+                }
+            }
+        }
+        switch (e.code) {
+            case 'Numpad9': {
+                clickLang('English');
+                break;
+            }
+            case 'Numpad7': {
+                clickLang('Русский');
+                break;
+            }
+            case 'Numpad5': {
+                document.querySelector('#oframecdnplayer > pjsdiv:nth-child(8) > pjsdiv:nth-child(1) > pjsdiv:nth-child(1)').click();
+                break;
+            }
+            // case 'Numpad4': {
+            //     window.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowLeft'}));
+            //     break;
+            // }
+            // case 'Numpad6': {
+            //     window.dispatchEvent(new KeyboardEvent(key, {key: 'ArrowRight'}));
+            //     break;
+            // }
         }
     };
 }
